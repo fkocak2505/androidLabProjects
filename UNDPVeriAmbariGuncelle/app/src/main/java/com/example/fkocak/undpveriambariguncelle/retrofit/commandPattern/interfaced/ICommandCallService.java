1957fc4a25a5be2e1938b9f820bede90a9d68f50
Fatih Koçak <fatih.kocak@medyasoft.com.tr>
2019-03-11 10:23:29 +0300

    updated

--- UNDPVeriAmbariGuncelle/app/src/main/java/com/example/fkocak/undpveriambariguncelle/retrofit/commandPattern/interfaced/ICommandCallService.java
+++
@@ -1,8 +0,0 @@
-package com.example.fkocak.undpveriambariguncelle.retrofit.commandPattern.interfaced;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-
-public interface ICommandCallService {
-    public <T> void callAsync(Call<T> tCall, Callback<T> callback);
-}
